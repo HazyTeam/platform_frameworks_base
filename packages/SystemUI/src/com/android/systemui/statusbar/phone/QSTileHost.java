0ae74c94e994513aef8be4c62341dbc1448e8310
Roman Birg <roman@cyngn.com>
2015-01-21 18:59:20 -0800

    Add NFC QS Tile
    More cleanup on QS tiles
    * Make strings & states more consistent to AOSP tiles
    * Remove unused/duplicate strings
    * Fix copyright headers
    
    Change-Id: Ib97f216c31c96e23d72ce7bf7e63b16b65f1bee0
    
    SystemUI: update quick tile animations and icons
    Use new AnimationIcons and ResourceIcon classes added with 5.1
    
    Change-Id: Ib904ac0cda2a2787f960893a3434cf21f392af3b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Modified for aosp QS tuner
    
    Change-Id: I5cefcd20391ffe8fb1bc40e92c522500e5315de8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -34,6 +34,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -232,6 +233,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
